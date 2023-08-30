13e0b8ef9634b0dd54895d2610b9c7bb12be62b2
EranDK <erangi94dhanushika@gmail.com>
2023-08-31 00:42:00 +0530

    create jobs , login, registers

--- src/main/java/com/mycompany/controller/controller.java
+++ src/main/java/com/mycompany/controller/controller.java
@@ -12,7 +12,27 @@ public class controller {
 
     @GetMapping("/about")
     public String about() {
+
         return "about";
+    }
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/jobs")
+    public String jobs(){
+        return "jobs";
     }
 }
 
