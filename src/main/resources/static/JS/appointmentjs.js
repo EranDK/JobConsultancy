f3498e7065b2546cdcc14b4628bf9320c98dbaea
EranDK <erangi94dhanushika@gmail.com>
2023-09-04 19:26:17 +0530

    updating

---
+++ src/main/resources/static/JS/appointmentjs.js
@@ -0,0 +1,5 @@
+document.getElementById('appointmentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // Add your logic here to process the form data or perform actions
+    alert('Form submitted successfully!');
+});
