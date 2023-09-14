5aa98072888425c9158546b7334cac8292753622
EranDK <erangi94dhanushika@gmail.com>
2023-09-14 23:25:27 +0530

    backend updating

--- src/main/java/com/mycompany/controller/BookingController.java
+++ src/main/java/com/mycompany/controller/BookingController.java
@@ -3,50 +3,74 @@ package com.mycompany.controller;
 import com.mycompany.model.Booking;
 import com.mycompany.model.Country;
 import com.mycompany.model.Job;
-import com.mycompany.repository.BookingRepository;
-import com.mycompany.repository.CountryRepository;
-import com.mycompany.repository.JobRepository;
+import com.mycompany.service.BookingService;
+import com.mycompany.service.CountryService;
+import com.mycompany.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
 public class BookingController {
-    @Autowired
-    private BookingRepository repo;
 
     @Autowired
-    CountryRepository countryrepo;
+    private CountryService countryService;
 
     @Autowired
-    JobRepository jobrepo;
+    private BookingService bookingService;
+
+    @Autowired
+    private JobService jobService;
 
     @GetMapping("/appointments")
     public String showAppointmentPage(Model model){
-        List<Country> listCountries= countryrepo.findAll();
+        List<Country> listCountries= countryService.getAllCountries();
         model.addAttribute("listCountries",listCountries);
-        List<Job>listJobs = jobrepo.findAll();
+        List<Job>listJobs = jobService.getAllJobs();
         model.addAttribute("listJobs", listJobs);
+        Booking booking = new Booking();
         model.addAttribute("booking", new Booking());
-
-
         return "appointments";
     }
 
-    @GetMapping("/history")
-    public String appointmentHistory(Model model){
-        List<Booking> listAppointments = repo.findAll();
-        model.addAttribute("listAppointments", listAppointments);
-        return "history";
-    }
-
     @PostMapping("/appointments/save")
-    public String saveBooking(Booking booking){
-        repo.save(booking);
+    public String saveAppointment(@ModelAttribute Booking booking){
+        bookingService.save(booking);
         return "redirect:/history";
     }
+
+    @GetMapping("/history")
+    public ModelAndView getAllBooking(){
+        List<Booking> listAppointments = bookingService.getAllBooking();
+        return new ModelAndView("history","listAppointments",listAppointments);
+    }
+
+    @RequestMapping("/deleteAppointmentList/{id}")
+    public String deleteAppointmentList(@PathVariable("id") int id){
+        bookingService.deleteById(id);
+        return "redirect:/history";
+    }
+
+    @RequestMapping("/editAppointment/{id}")
+    public String editCountry(@PathVariable("id") int id, Model model){
+        Booking booking = bookingService.getBookingById(id);
+        List<Country>listCountries= countryService.getAllCountries();
+        model.addAttribute("listCountries",listCountries);
+        List<Job>listJobs = jobService.getAllJobs();
+        model.addAttribute("listJobs", listJobs);
+        model.addAttribute("booking", booking);
+        return "appointmentsedit";
+    }
+
+
+    @PostMapping("/appointments/update")
+    public String updateAppointment(@ModelAttribute Booking booking){
+        bookingService.save(booking);
+        return "redirect:/history";
+    }
+
 }
